5c649d64dad6138260799035fc5e7c99f42e898b
KaeBae <kiran@blox.is>
2021-06-10 21:11:43 +0100

    code organisation

--- contracts/CrossChainSale/Callback.sol
+++ contracts/CrossChainSale/Callback.sol
@@ -1,6 +1,7 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./interfaces/ICallback.sol";
+import "../interfaces/ICallback.sol";
 
 abstract contract Callback is ICallback{
     address public authorisedReceiver;
